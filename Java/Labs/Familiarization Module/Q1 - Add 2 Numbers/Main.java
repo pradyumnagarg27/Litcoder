5ab7937ffa59a9702b4faa4a9c2c5b4a0a3ca0b0
pradyumnagarg27 <pradyumnagrg@gmail.com>
2023-11-29 00:23:58 +0530

    Familiarization Module

---
+++ Java/Labs/Familiarization Module/Q1 - Add 2 Numbers/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int ret = add(a,b);        
+        System.out.println(ret);
+    }
+    
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
+                                                                                                                            
